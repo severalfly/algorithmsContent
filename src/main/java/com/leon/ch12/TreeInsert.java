d8a16b046c65ae209da792c431f096a9dfd8a49c
张云飞 <zhangsmile90@gmail.com>
2018-07-25 06:25:24 +0800

    tree insert

--- src/main/java/com/leon/ch12/TreeInsert.java
+++ src/main/java/com/leon/ch12/TreeInsert.java
@@ -1,5 +1,59 @@
 package com.leon.ch12;
 
+import org.junit.Test;
+
 public class TreeInsert
 {
+	/**
+	 * 把 z 插入到 T中
+	 * @param t tree
+	 * @param z 待插入的节点
+	 */
+	private void treeInsert(TreeW t, TreeW z)
+	{
+		TreeW y = null;
+		TreeW x = t;
+		while (x != null)
+		{
+			y = x;
+			if (z.getKey() < x.getKey())
+			{
+				x = x.getLeft();
+			}
+			else
+			{
+				x = x.getRight();
+			}
+		}
+		z.setP(y);
+		if (y == null)
+		{
+			// 原始tree 为null
+			t = z;
+		}
+		else if (y.getKey() > z.getKey())
+		{
+			y.setLeft(z);
+		}
+		else
+		{
+			y.setRight(z);
+		}
+	}
+
+	@Test
+	public void testTreeInsert()
+	{
+		TreeW treeW = TreeUtils.buildTreeW();
+		TreeUtils.printTree(treeW);
+		System.out.println();
+		TreeW z = new TreeW(5);
+		treeInsert(treeW, z);
+
+		System.out.println(treeW.getKey());
+		// 打印看看结果
+		TreeUtils.printTree(treeW);
+		System.out.println();
+
+	}
 }
--- src/main/java/com/leon/ch12/TreeUtils.java
+++ src/main/java/com/leon/ch12/TreeUtils.java
@@ -6,9 +6,23 @@ public class TreeUtils
 	{
 		TreeW treeW = new TreeW(2);
 		TreeW treeW2 = new TreeW(1);
-		TreeW treeW3 = new TreeW(3);
+		TreeW treeW3 = new TreeW(6);
 		treeW.setLeft(treeW2);
 		treeW.setRight(treeW3);
 		return treeW;
+	}
+
+	/**
+	 * 中序打印
+	 * @param treeW 待打印
+	 */
+	public static void printTree(TreeW treeW)
+	{
+		if (treeW != null)
+		{
+			printTree(treeW.getLeft());
+			System.out.print(treeW.getKey() + " ");
+			printTree(treeW.getRight());
+		}
 	}
 }
