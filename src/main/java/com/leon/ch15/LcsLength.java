5bbe83c261ff25e7bc85cf03a575a11991d1e75a
张云飞 <zhangsmile90@gmail.com>
2018-08-02 07:44:54 +0800

    simple lcs length

---
+++ src/main/java/com/leon/ch15/LcsLength.java
@@ -0,0 +1,57 @@
+package com.leon.ch15;
+
+import com.leon.utils.DataOper;
+import org.junit.Test;
+
+public class LcsLength
+{
+	private int[] xData = new int[] { 1, 2, 3, 2, 4, 1, 2, 0 };
+	private int[] yData = new int[] { 2, 4, 3, 1, 2, 1, 0 };
+
+	private int lcsLength(int[] x, int[] y)
+	{
+		int m = x.length - 1;
+		int n = y.length - 1;
+		int[][] b = new int[m + 1][n + 1];
+		int[][] c = new int[m + 1][n + 1];
+		for (int i = 1; i <= m; i++)
+		{
+			c[i][0] = 0;
+		}
+		for (int j = 0; j <= n; j++)
+		{
+			c[0][j] = 0;
+		}
+
+		for (int i = 1; i <= m; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (x[i] == y[j])
+				{
+					c[i][j] = c[i - 1][j - 1] + 1;
+					b[i][j] = 2;
+				}
+				else if (c[i - 1][j] >= c[i][j - 1])
+				{
+					c[i][j] = c[i - 1][j];
+					b[i][j] = 1;
+				}
+				else
+				{
+					c[i][j] = c[i][j - 1];
+					b[i][j] = 0;
+				}
+			}
+		}
+		DataOper.print(c);
+		DataOper.print(b);
+		return 0;
+	}
+
+	@Test
+	public void testLcsLength()
+	{
+		lcsLength(this.xData, this.yData);
+	}
+}
