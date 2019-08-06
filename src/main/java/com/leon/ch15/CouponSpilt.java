31059ee7e2fcfcc6fe4a3077f1e6f762a7cae85a
张云飞 <zhangsmile90@gmail.com>
2019-08-06 20:50:48 +0800

    修改支持

--- src/main/java/com/leon/ch15/CouponSpilt.java
+++ src/main/java/com/leon/ch15/CouponSpilt.java
@@ -1,6 +1,5 @@
 package com.leon.ch15;
 
-import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
 /**
@@ -8,109 +7,17 @@ import org.junit.Test;
  */
 public class CouponSpilt
 {
+	// 这里300 为最大订单金额，可以扩展到很大很大
 	private static int[] pData = new int[3000];
 
 	static
 	{
+		// 存在哪些面值的优惠券，对应设置为对应值就可以
 		//		pData[9] = 9;
 		//		pData[99] = 99;
 		pData[199] = 199;
 		pData[299] = 299;
 		pData[399] = 399;
-	}
-
-	private int curRod(int[] p, int n)
-	{
-		if (n == 0)
-		{
-			return 0;
-		}
-		int q = 0 - Integer.MAX_VALUE;
-		for (int i = 1; i <= n; i++)
-		{
-			int tmp = p[i] + curRod(p, n - i);
-			q = Math.max(q, tmp);
-		}
-		return q;
-	}
-
-	//	@Test
-	//	public void testCutRod()
-	//	{
-	//		long time = System.currentTimeMillis();
-	//		int q = curRod(this.pData, 1000);
-	//		long dtime = System.currentTimeMillis() - time;
-	//		System.out.println(q + "  " + dtime);
-	//	}
-	//
-	//	@Test
-	//	public void testMemoizedCutRod()
-	//	{
-	//		long time = System.currentTimeMillis();
-	//		int q = memoizedCutRod(pData, 1000);
-	//		long dtime = System.currentTimeMillis() - time;
-	//		System.out.println(q + "  " + dtime);
-	//	}
-
-	private int memoizedCutRod(int[] p, int n)
-	{
-		int[] r = new int[n + 1];
-		for (int i = 0; i <= n; i++)
-		{
-			r[i] = 0 - Integer.MAX_VALUE;
-		}
-		return memoizedCutRodAux(p, n, r);
-	}
-
-	private int memoizedCutRodAux(int[] p, int n, int[] r)
-	{
-		if (r[n] >= 0)
-		{
-			return r[n];
-		}
-		int q = 0 - Integer.MAX_VALUE;
-		if (n == 0)
-		{
-			q = 0;
-		}
-		else
-		{
-			for (int i = 1; i <= n; i++)
-			{
-				int tmp = p[i] + memoizedCutRodAux(p, n - i, r);
-				q = Math.max(q, tmp);
-			}
-		}
-		r[n] = q;
-		return q;
-	}
-
-	@Test
-	public void testBottomUpCutRod()
-	{
-		long time = System.currentTimeMillis();
-		int q = bottomUpCutRod(pData, 1000);
-		long dtime = System.currentTimeMillis() - time;
-		System.out.println(q + "  " + dtime);
-	}
-
-	private int bottomUpCutRod(int[] p, int n)
-	{
-		int[] r = new int[n + 1];
-		for (int i = 0; i < n + 1; i++)
-		{
-			r[i] = 0;
-		}
-		for (int j = 1; j <= n; j++)
-		{
-			int q = Integer.MIN_VALUE;
-			for (int i = 1; i <= j; i++)
-			{
-				q = Math.max(q, p[i] + r[j - i]);
-			}
-			r[j] = q;
-		}
-		return r[n];
 	}
 
 	private SAndR extendBottomUpCutRod(int[] p, int n)
@@ -140,10 +47,11 @@ public class CouponSpilt
 	@Test
 	public void testExtendBottomUpCutRod()
 	{
-		int n = 1000;
+		int n = 1993;
 		SAndR sAndR = extendBottomUpCutRod(pData, n);
 
-		System.out.println(JSONObject.toJSONString(sAndR));
+		//		System.out.println(JSONObject.toJSONString(sAndR.getR()));
+		//		System.out.println(JSONObject.toJSONString(sAndR.getS()));
 		while (n > 0)
 		{
 			System.out.println(sAndR.getS()[n]);
